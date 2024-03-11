c5fadffe5118fd6517c9a5c212b663570e73a79a
Ion Leu <leuionofficial@gmail.com>
2024-03-11 16:43:50 +0200

    add styles with prettier

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -2,15 +2,15 @@
 import styled from '@emotion/styled'
 
 const PageLayout = ({ children }: { children: React.ReactNode }) => {
-    return <Container>{children}</Container>
+  return <Container>{children}</Container>
 }
 
 const Container = styled.div({
-    height: '100vh',
-    padding: '60px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+  height: '100vh',
+  padding: '60px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
 })
 
 export default PageLayout
