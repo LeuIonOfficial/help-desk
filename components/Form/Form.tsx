4d1dd8941375a6ebe3963dd6339ca449a6e3f4de
Ion Leu <leuionofficial@gmail.com>
2024-03-11 15:34:11 +0200

    refactor form component

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,9 +1,9 @@
 'use client'
 import useFormHook from '@/hooks/useFormHook'
 import { Grid } from '../layout/Grid'
-import { TextArea } from '../inputs/TextArea'
 
 import * as S from './styled'
+import TextField from '@/components/inputs/TextField'
 
 const Form = () => {
   const { methods, onSubmit } = useFormHook()
@@ -17,43 +17,43 @@ const Form = () => {
         gridRowGap="10px"
         columnGap="10px"
       >
-        <TextArea
+        <TextField
           gridRow="1"
           label="Application URL"
           inputId=""
           fullWidth
           register={register('applicationUrl')}
-        ></TextArea>
+        ></TextField>
         <S.Heading gridRow="2">Authentication Security Settings</S.Heading>
-        <TextArea
+        <TextField
           gridRow="3"
           label="JWT Secret TTL:"
           inputId=""
           fullWidth
           register={register('JWT_SECRET_TTL')}
-        ></TextArea>
-        <TextArea
+        ></TextField>
+        <TextField
           gridRow="3"
           label="Refresh Secret TTL:"
           inputId=""
           fullWidth
           register={register('REFRESH_SECRET_TTL')}
-        ></TextArea>
+        ></TextField>
         <S.Heading gridRow="4">Email Settings:</S.Heading>
-        <TextArea
+        <TextField
           gridRow="5"
           label="Email host:"
           inputId=""
           fullWidth
           register={register('EMAIL_HOST')}
-        ></TextArea>
-        <TextArea
+        ></TextField>
+        <TextField
           gridRow="5"
           label="Email Port:"
           inputId=""
           fullWidth
           register={register('EMAIL_PORT')}
-        ></TextArea>
+        ></TextField>
       </Grid>
     </S.Form>
   )
