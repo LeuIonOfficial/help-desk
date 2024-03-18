488165496f239c2c6e441e105901a81f658e85e1
Ion Leu <leuionofficial@gmail.com>
2024-03-18 09:10:51 +0200

    Refactor Checkbox component to use forwardRef

--- components/inputs/Checkbox/Checkbox.tsx
+++ components/inputs/Checkbox/Checkbox.tsx
@@ -1,38 +1,33 @@
 import { Checkbox as MuiCheckbox, FormControlLabel } from '@mui/material'
-import { forwardRef } from 'react'
+import { Ref, forwardRef } from 'react'
 import { PropsType } from './types'
 
-export const Checkbox = forwardRef(
-  (
-    {
-      label,
-      name,
-      value,
-      disabled = false,
-      size = 'small',
-      onChange,
-      checked,
-    }: PropsType,
-    ref
-  ) => {
-    console.log('Checkbox', value)
-    return (
-      <FormControlLabel
-        control={
-          <MuiCheckbox
-            value={value}
-            size={size}
-            disabled={disabled}
-            onChange={onChange}
-            checked={checked}
-          />
-        }
-        label={label}
-        name={name}
-        ref={ref}
+const CheckboxComponent = (
+  {
+    label,
+    name,
+    value,
+    disabled = false,
+    size = 'small',
+    onChange,
+    checked,
+  }: PropsType,
+  ref: Ref<HTMLInputElement>
+) => (
+  <FormControlLabel
+    control={
+      <MuiCheckbox
+        value={value}
+        size={size}
+        disabled={disabled}
+        onChange={onChange}
+        checked={checked}
       />
-    )
-  }
+    }
+    label={label}
+    name={name}
+    ref={ref}
+  />
 )
 
-Checkbox.displayName = 'Checkbox'
+export const Checkbox = forwardRef(CheckboxComponent)
