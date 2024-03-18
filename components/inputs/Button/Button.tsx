f0daf3314ff59dd8ca5a150758e2c7f59a444ce0
Ion Leu <leuionofficial@gmail.com>
2024-03-18 09:11:57 +0200

    Refactor Button component to use forwardRef

--- components/inputs/Button/Button.tsx
+++ components/inputs/Button/Button.tsx
@@ -1,19 +1,24 @@
 import { Button as MuiButton, CircularProgress } from '@mui/material'
 import { Props } from './types'
+import { Ref, forwardRef } from 'react'
 
-export const Button = ({
-  variant = 'contained',
-  size = 'medium',
-  children = '',
-  isLoading = false,
-  iconSize = 24,
-  iconColor = 'inherit',
-  disabled = false,
-  onClick,
-  type = 'button',
-}: Props) => {
+const ButtonComponent = (
+  {
+    variant = 'contained',
+    size = 'medium',
+    children = '',
+    isLoading = false,
+    iconSize = 24,
+    iconColor = 'inherit',
+    disabled = false,
+    onClick,
+    type = 'button',
+  }: Props,
+  ref: Ref<HTMLButtonElement>
+) => {
   return (
     <MuiButton
+      ref={ref}
       onClick={onClick}
       disabled={isLoading || disabled}
       size={size}
@@ -27,3 +32,5 @@ export const Button = ({
     </MuiButton>
   )
 }
+
+export const Button = forwardRef(ButtonComponent)
